be46c9c849e2a733d083abb94b95ae06a38b284e
Naveen S <coolnaveen1996@gmail.com>
2020-09-11 21:26:23 +0530

    initial server changes

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(8080, (req,err) => {
+    console.log("Server Started At Port", 8080);
+}); 
+
+app.get("/", (req,res) => {
+    res.send("Hello, World!!");
+})
