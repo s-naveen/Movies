e19c0260794193f041cebe4f6b476f211845ba8f
Naveen S <coolnaveen1996@gmail.com>
2020-09-13 16:34:31 +0530

    typo

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -29,7 +29,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Nowss
+          Learn React Now
         </a>
       </header>
     </div>
